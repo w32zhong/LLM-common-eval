0d9dfd3d63e9ffcfb644edd99b9ecb41d35ba56f
t-k- <clock126@126.com>
2023-12-30 18:32:34 -0500

    expand eval_mistral/mistral_on_SNLI.sh

--- eval_mistral/mistral_on_SNLI.sh
+++ eval_mistral/mistral_on_SNLI.sh
@@ -7,3 +7,14 @@ export CUDA_VISIBLE_DEVICES=6
 detached_experiment exp2 \
     python eval_mistral/mistral_on_SNLI_greedy_fewshot.py --shots 1
 waitfor_experiments exp1 exp2
+
+export CUDA_VISIBLE_DEVICES=2
+detached_experiment exp3 \
+    python eval_mistral/mistral_on_SNLI_greedy_fewshot_4bit.py --shots 5
+export CUDA_VISIBLE_DEVICES=3
+detached_experiment exp4 \
+    python eval_mistral/mistral_on_SNLI_greedy_fewshot_4bit.py --shots 3
+export CUDA_VISIBLE_DEVICES=4
+detached_experiment exp5 \
+    python eval_mistral/mistral_on_SNLI_greedy_fewshot_4bit.py --shots 1
+waitfor_experiments exp3 exp4 exp5
