54e13ba3c2bbec6f26a3e211ff8b5584363c4f0b
t-k- <clock126@126.com>
2023-12-31 13:38:03 -0500

    update eval_mistral/mistral_on_SuperGLUE_all.sh

--- eval_mistral/mistral_on_SuperGLUE_all.sh
+++ eval_mistral/mistral_on_SuperGLUE_all.sh
@@ -10,29 +10,31 @@ source ./llm_common_eval/batch_eval.sh
 # multirc   4.85K
 # record    10K
 
+export CUDA_VISIBLE_DEVICES=0
 detached_experiment exp1 \
     python eval_mistral/mistral_on_SuperGLUE_all.py --dataset cb
 waitfor_experiments exp1
 
+export CUDA_VISIBLE_DEVICES=0
 detached_experiment exp2 \
     python eval_mistral/mistral_on_SuperGLUE_all.py --dataset copa
-waitfor_experiments exp2
+export CUDA_VISIBLE_DEVICES=1
 detached_experiment exp3 \
     python eval_mistral/mistral_on_SuperGLUE_all.py --dataset wsc
-waitfor_experiments exp3
-
+export CUDA_VISIBLE_DEVICES=2
 detached_experiment exp4 \
     python eval_mistral/mistral_on_SuperGLUE_all.py --dataset rte
+export CUDA_VISIBLE_DEVICES=3
 detached_experiment exp5 \
     python eval_mistral/mistral_on_SuperGLUE_all.py --dataset wic
-waitfor_experiments exp4 exp5
-
+export CUDA_VISIBLE_DEVICES=4
 detached_experiment exp6 \
     python eval_mistral/mistral_on_SuperGLUE_all.py --dataset boolq
+export CUDA_VISIBLE_DEVICES=5
 detached_experiment exp7 \
     python eval_mistral/mistral_on_SuperGLUE_all.py --dataset multirc
-waitfor_experiments exp6 exp7
+waitfor_experiments exp2 exp3 exp4 exp5 exp6 exp7
 
-detached_experiment exp8 \
-    python eval_mistral/mistral_on_SuperGLUE_all.py --dataset record
-waitfor_experiments exp8
+#detached_experiment exp8 \
+#    python eval_mistral/mistral_on_SuperGLUE_all.py --dataset record
+#waitfor_experiments exp8
