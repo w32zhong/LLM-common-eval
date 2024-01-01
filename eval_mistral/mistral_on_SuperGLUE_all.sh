dd5970d01a3510038bb008794c20324448c19503
t-k- <clock126@126.com>
2024-01-01 03:14:11 +0000

    added eval_mistral/mistral_on_SuperGLUE_all_4bit.py

--- eval_mistral/mistral_on_SuperGLUE_all.sh
+++ eval_mistral/mistral_on_SuperGLUE_all.sh
@@ -1,4 +1,5 @@
 source ./llm_common_eval/batch_eval.sh
+SCRIPT=eval_mistral/mistral_on_SuperGLUE_all_4bit.py
 
 # Dataset validation size (ordered):
 # cb        56
@@ -12,29 +13,29 @@ source ./llm_common_eval/batch_eval.sh
 
 export CUDA_VISIBLE_DEVICES=0
 detached_experiment exp1 \
-    python eval_mistral/mistral_on_SuperGLUE_all.py --dataset cb
+    python $SCRIPT --dataset cb
 waitfor_experiments exp1
 
 export CUDA_VISIBLE_DEVICES=0
 detached_experiment exp2 \
-    python eval_mistral/mistral_on_SuperGLUE_all.py --dataset copa
+    python $SCRIPT --dataset copa
 export CUDA_VISIBLE_DEVICES=1
 detached_experiment exp3 \
-    python eval_mistral/mistral_on_SuperGLUE_all.py --dataset wsc
+    python $SCRIPT --dataset wsc
 export CUDA_VISIBLE_DEVICES=2
 detached_experiment exp4 \
-    python eval_mistral/mistral_on_SuperGLUE_all.py --dataset rte
+    python $SCRIPT --dataset rte
 export CUDA_VISIBLE_DEVICES=3
 detached_experiment exp5 \
-    python eval_mistral/mistral_on_SuperGLUE_all.py --dataset wic
+    python $SCRIPT --dataset wic
 export CUDA_VISIBLE_DEVICES=4
 detached_experiment exp6 \
-    python eval_mistral/mistral_on_SuperGLUE_all.py --dataset boolq
+    python $SCRIPT --dataset boolq
 export CUDA_VISIBLE_DEVICES=5
 detached_experiment exp7 \
-    python eval_mistral/mistral_on_SuperGLUE_all.py --dataset multirc
+    python $SCRIPT --dataset multirc
 waitfor_experiments exp2 exp3 exp4 exp5 exp6 exp7
 
 #detached_experiment exp8 \
-#    python eval_mistral/mistral_on_SuperGLUE_all.py --dataset record
+#    python $SCRIPT --dataset record
 #waitfor_experiments exp8
