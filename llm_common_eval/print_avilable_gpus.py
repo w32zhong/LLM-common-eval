00d877c8c19646e3ee5076e4bf0ca96223118168
w32zhong <clock126@126.com>
2024-04-20 16:27:49 -0400

    print_available_gpus(): added min_usage arg.

--- llm_common_eval/print_avilable_gpus.py
+++ llm_common_eval/print_avilable_gpus.py
@@ -4,16 +4,15 @@ import os
 from torch import cuda
 
 
-def print_available_gpus(total=None, n=None,
+def print_available_gpus(total=None, n=None, min_usage=1,
     print_used_count=False, print_to_file='/dev/stdout'):
     total = cuda.device_count() if total is None else total
     all_gpus = [cuda.device(i) for i in range(total)]
     avail_gpus = []
     for i, gpu in enumerate(all_gpus):
         info = cuda.mem_get_info(device=gpu)
-        avail_GiB = info[0] / (1024 * 1024 * 1024)
-        total_GiB = info[1] / (1024 * 1024 * 1024)
-        if avail_GiB > total_GiB // 2:
+        used_GiB = (info[1] - info[0]) / (1024 * 1024 * 1024)
+        if used_GiB < min_usage:
             avail_gpus.append(str(i))
     if n is None: n = total
     result = avail_gpus[:n]
