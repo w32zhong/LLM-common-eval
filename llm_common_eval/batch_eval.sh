e7154e6777cf6aee1a1f97c0bdf70e34467a8cd4
w32zhong <clock126@126.com>
2025-05-02 19:40:01 +0000

    +function kill_all_exp_sessions()

--- llm_common_eval/batch_eval.sh
+++ llm_common_eval/batch_eval.sh
@@ -59,3 +59,7 @@ function block_until_set_available_devices() {
     done
     export CUDA_VISIBLE_DEVICES=$devs
 }
+
+function kill_all_exp_sessions() {
+    tmux list-sessions -F '#S' -f '#{m:dat*,#S}' | xargs -n 1 tmux kill-session -t
+}
